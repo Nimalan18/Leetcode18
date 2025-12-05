da3dc445f649145c270934c208de1269d74f0250
Nimalan Arasu S <nimalanarasusenthil@gmail.com>
2025-12-05 09:33:39 +0530

    Add countPartitions method for even sum difference
    Implement countPartitions method to count valid partitions.

---
+++ Day41.java
@@ -0,0 +1,8 @@
+//3432. Count Partitions with Even Sum Difference
+
+class Solution {
+    public int countPartitions(int[] A) {
+        int total = Arrays.stream(A).sum();
+        return (total & 1) == 0 ? A.length - 1 : 0;
+    }
+}
